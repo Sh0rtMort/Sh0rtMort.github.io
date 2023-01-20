f441a7d372e435034167443918b36560b06fbd2d
SaltWater <97995798+Sh0rtMort@users.noreply.github.com>
2023-01-20 10:19:23 -0600

    new script

--- indexScript.js
+++ indexScript.js
@@ -1,5 +1,3 @@
-const fader = document.querySelector('.fade-in');
-const diagram = document.querySelector('.diagram');
 
 setTimeout(function() {
   document.querySelector('.loader-wrapper').style.display = "none";
@@ -16,64 +14,17 @@ setTimeout(function() {
 
 window.onload = function() {
   window.scrollTo(0, 0);
-  fader.style.opacity = "0";
-  fader.style.transform = "translateX(-100px)";
-  diagram.style.opacity = "0";
-  diagram.style.transform = "translateY(-100px)";
-
 }
 
 setTimeout(function() {
-  fader.style.opacity = "1";
-  fader.style.transform = "translateX(0px)";
-  diagram.style.opacity = "1";
-  diagram.style.transform = "translateY(0px)";
   clearTimeout(2000);
 } ,2000);
 
+const sizeTest = document.querySelector('.size-test');
+const infill = document.querySelector('.infill');
 
-window.addEventListener('scroll', function() {
-  if (window.scrollY <= 120) {
-    fader.style.opacity = "1";
-    fader.style.transform = "translateX(0px)";
-    diagram.style.opacity = "1";
-    diagram.style.transform = "translateY(0px)";
-  } else {
-    fader.style.opacity = "0";
-    fader.style.transform = "translateX(-100px)";
-    diagram.style.opacity = "0";
-    diagram.style.transform = "translateY(-100px)";
-  }
-});
-
-
-
-const p1 = document.querySelector('.p1-wrapper');
-const hl1 = document.querySelector('.home-logo1');
-const col12 = document.querySelector('.col-12');
-
-
-window.addEventListener('scroll', function() {
-
-  const middle = (window.innerHeight / 2) - 20;
-  const middleLow = (window.innerHeight / 2) + 550;
-
-  if (window.scrollY >= middle && window.scrollY <= middleLow) {
-    p1.style.opacity = "1";
-    p1.style.transform = "translateX(0px)";
-    hl1.style.opacity = "1";
-    hl1.style.display = "block";
-    hl1.style.transform = "translateX(0px)";
-    col12.style.opacity = "1";
-    col12.style.transform = "translateY(0px)";
-  } else {
-    p1.style.opacity = "0";
-    p1.style.transform = "translateX(-600px)";
-    hl1.style.opacity = "0";
-    hl1.style.display = "none";
-    // hl1.style.transform = "translateX(600px)";
-    col12.style.opacity = "0";
-    col12.style.transform = "translateY(-100px)";
-    document.body.style.overflowX = "hidden";
-  }
+sizeTest.addEventListener('click' ,function() {
+  console.log(window.innerWidth);
+  const width = window.innerWidth;
+  infill.innerHTML = width;
 });
